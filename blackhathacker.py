e5ac34cb277eb01f9089ac94439b9fd758bd9b3d
bob34-byte <78810546+bob34-byte@users.noreply.github.com>
2021-06-11 11:22:03 -0400

    Update blackhathacker.py

--- blackhathacker.py
+++ blackhathacker.py
@@ -1,4 +1,8 @@
 import pygame
 pygame.init()
 screen = pygame.display.set_mode(500,500)
-running =  True
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type = pygame.QUIT:
+      running = False
