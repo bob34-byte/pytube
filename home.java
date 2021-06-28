95965dab79aed2dc3b0a365f1e10132359d29b76
bob34-byte <78810546+bob34-byte@users.noreply.github.com>
2021-06-28 09:20:53 -0400

    Update home.java

--- home.java
+++ home.java
@@ -1,7 +1,8 @@
 public class Main {
    public static void main(String[] args){
       var name = "bob";
-      private String name = "billy";
+      public  String name = "billy";
+      
     }
  }
 }
